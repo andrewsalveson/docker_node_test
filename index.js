b49f0d56b66f368ca14fd851d5983adeb22aa7bd
Andrew Salveson <andrewsalveson@gmail.com>
2015-11-17 18:07:34 +0000

    example bug fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function(req,res){
 
 app.get('/test', function(req,res){
 	res.send('this is a test folder\n');
-}
+});
 
 app.listen(PORT);
 console.log('Running on http://localhost:'+PORT);
